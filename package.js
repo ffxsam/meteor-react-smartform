213e3db444b8088e5ed00ddd5d78f44539df25c9
Sam <samh@ff-formula.com>
2015-11-14 19:31:30 -0800

    Syncing package version with GitHub release ver

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ffxsam:react-smartform',
-  version: '0.7.0',
+  version: '0.7.2',
   summary: 'Smart form elements for React',
   git: 'https://github.com/ffxsam/meteor-react-smartform',
   documentation: 'README.md'
